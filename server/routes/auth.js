b1b82281a6648b0cc93a38bb5dc559f327b80bf1
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2023-03-28 23:02:00 -0500

    created and updated auth.js; file sits in routes folder and will hold all routes for site

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
 const router = express.Router();
 
-app.get('/api/signup', (req, res) => {
+router.get('/signup', (req, res) => {
     res.json({
-        data: 'YAY!!! You hit the signup endpoint! GREAT JOB!!!'
-    })
-})
+        data: 'YAY!!! You hit the signup endpoint from the ROUTER! GREAT JOB!!!'
+    });
+});
+
+router.get('/signin', (req, res) => {
+    res.json({
+        data: 'Here is the signin page!'
+    });
+});
+
+router.get('/forgotPassword', (req, res) => {
+    res.json({
+        data: 'Did you forget your password? Reset here!'
+    });
+});
+
+module.exports = router; // {} by default
