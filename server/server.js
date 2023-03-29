4dd12874849226bee1d38125515c8f93748d45d4
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2023-03-28 23:03:44 -0500

    updated server.js in structuring server file; removed route and replaced with .use

--- server/server.js
+++ server/server.js
@@ -7,9 +7,15 @@ const express = require('express');
 // allows us to access all functionalities express provides
 const app = express();
 
+// Import Routes
+const authRoutes = require('./routes/auth');
+
+// middleware
+app.use('/api', authRoutes);
+
 // creating const to hold port we are using
-const port = process.env.port || 8000
+const port = process.env.port || 8000;
 
 app.listen(port, () => {
     console.log(`API is running on port ${port}!`)
-})
+});
