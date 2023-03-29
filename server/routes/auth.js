b9b254e35ceab76e416c2a64adb09c456ddc4568
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2023-03-29 15:48:05 -0500

    removed routes and placed them in controllers auth.js; created import for controller and cleaning up get function.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const router = express.Router();
+// Import Controller
+const {signup} = require('../controllers/auth');
 
-router.get('/signup', (req, res) => {
-    res.json({
-        data: 'YAY!!! You hit the signup endpoint from the ROUTER! GREAT JOB!!!'
-    });
-});
-
-router.get('/signin', (req, res) => {
-    res.json({
-        data: 'Here is the signin page!'
-    });
-});
-
-router.get('/forgotPassword', (req, res) => {
-    res.json({
-        data: 'Did you forget your password? Reset here!'
-    });
-});
+router.get('/signup', signup);
 
 module.exports = router; // {} by default
