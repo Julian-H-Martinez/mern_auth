49324e579c0fff675d8a025884ebe932aacac40e
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2023-03-30 10:34:04 -0500

    added user schema; moving to virtual field portion

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,48 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+
 // user schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 32,
+        lowercase: true,
+        unique: true
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+    },
+    salt: String, 
+    role: {
+        type: String,
+        default: 'subscriber'
+    },
+    resetPasswordLink: {
+        type: String,
+        default: 'subscriber'
+    }
+}, {timestamps: true});
 
 // virtual field
+// userSchema.virtual('password')
+// .set(function(password){
+//     this._password = password
+//     this.salt = this.makeSalt()
+//     this.hashed_password = this.encryptPassword(password)
+// })
+// .get(function(){
+//     return this._password
+// })
+
+
 
 // methods
